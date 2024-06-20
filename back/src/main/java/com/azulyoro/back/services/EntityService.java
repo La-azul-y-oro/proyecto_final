c5dd95ec14291aff0191bfe258ad24fdb60c4432
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-06-19 21:48:47 -0300

    feat(CU-86b0yt3d3): add generic interface to be use by entities services

---
+++ back/src/main/java/com/azulyoro/back/services/EntityService.java
@@ -0,0 +1,14 @@
+package com.azulyoro.back.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+interface EntityService<T, R>{
+    R create(T t);
+    R getById(Long id);
+    List<R> getAll();
+    Page<R> getByPage(Pageable pageable);
+    Void delete(Long id);
+}
