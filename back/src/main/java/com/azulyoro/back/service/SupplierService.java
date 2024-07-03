4bd0dd511f25995d7cdb6354c7f327d3e22b9534
nicod1889 <niconicodinolfo@gmail.com>
2024-07-03 19:07:52 -0300

    feat(CU-86b0yt572) Add supplier entity

---
+++ back/src/main/java/com/azulyoro/back/service/SupplierService.java
@@ -0,0 +1,37 @@
+package com.azulyoro.back.service;
+
+import com.azulyoro.back.model.Supplier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupplierService implements EntityService<Supplier, Supplier> {
+    
+    @Override
+    public Supplier create(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public Supplier getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Page<Supplier> getByPage(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Void delete(Long id) {
+        return null;
+    }
+}
