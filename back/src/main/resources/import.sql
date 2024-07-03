91c1e06c25a65c60d107b05908a1e996bb951d4a
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-07-03 20:33:54 -0300

    feat(CU-86b14tdaj): add paymentType data

---
+++ back/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into payment_type (name, category) values ('Visa', 'DEBIT_CARD');
+insert into payment_type (name, category) values ('Visa', 'CREDIT_CARD');
+insert into payment_type (name, category) values ('Mastercard', 'DEBIT_CARD');
+insert into payment_type (name, category) values ('Mastercard', 'CREDIT_CARD');
+insert into payment_type (name, category) values ('American Express', 'CREDIT_CARD');
+insert into payment_type (name, category) values ('Efectivo', 'OTHER');
+insert into payment_type (name, category) values ('Transferencia', 'OTHER');
+
