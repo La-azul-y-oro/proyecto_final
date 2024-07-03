ccb7fc8874f9ed7244f80f5dd2c5ef7e7812b1e8
Juan <juanschiavoni713@gmail.com>
2024-07-03 18:55:49 -0300

    feat(CU-86b0yt1af) Add entity Pay

---
+++ back/src/main/java/com/azulyoro/back/service/PayService.java
@@ -0,0 +1,37 @@
+package com.azulyoro.back.service;
+
+import com.azulyoro.back.model.Pay;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PayService implements EntityService<Pay, Pay>{
+
+    @Override
+    public Pay create(Pay pay) {
+        return null;
+    }
+
+    @Override
+    public Pay getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Pay> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public Page<Pay> getByPage(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Void delete(Long id) {
+        return null;
+    }
+}
