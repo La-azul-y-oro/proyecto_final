eaca7789a25ce2cfce9a6ddb211631e3fb030584
nicod1889 <niconicodinolfo@gmail.com>
2024-07-04 00:16:51 -0300

    feat(CU-86b0yt41y) Add Brand entity

---
+++ back/src/main/java/com/azulyoro/back/model/Brand.java
@@ -0,0 +1,25 @@
+package com.azulyoro.back.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private BrandCategory category;
+}
