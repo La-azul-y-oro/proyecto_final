6fd91260492a2aaa26064b534f51d2f8f692070f
Nicolás Dinolfo <153851997+nicod1889@users.noreply.github.com>
2024-07-04 09:34:05 -0300

    Create BrandRepository.java

---
+++ back/src/main/java/com/azulyoro/back/repository/BrandRepository.java
@@ -0,0 +1,10 @@
+package com.azulyoro.back.repository;
+
+import com.azulyoro.back.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+}
