5eb859827f852e42cb4afcf5e5087e7194bff4f8
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-07-07 11:31:34 -0300

    feat(CU-86b161dkb): add update method to service interface. Update services implementations

--- back/src/main/java/com/azulyoro/back/service/InventoryService.java
+++ back/src/main/java/com/azulyoro/back/service/InventoryService.java
@@ -3,13 +3,20 @@ package com.azulyoro.back.service;
 import com.azulyoro.back.model.Inventory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class InventoryService implements EntityService <Inventory, Inventory> {
 
     @Override
     public Inventory create(Inventory inventory) {
+        return null;
+    }
+
+    @Override
+    public Inventory update(Inventory inventory) {
         return null;
     }
 
