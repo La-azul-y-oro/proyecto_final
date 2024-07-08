916f773cf213adf579213836a0e7eddcca1b4063
Santiago Ceron <ceronsanti@gmail.com>
2024-07-08 19:02:51 -0300

    feat(CU-86b0yt4p3): created SparePart entity

---
+++ back/src/main/java/com/azulyoro/back/model/SparePart.java
@@ -0,0 +1,30 @@
+package com.azulyoro.back.model;
+
+import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Setter
+public class SparePart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    private String madeIn;
+}
