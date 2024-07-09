e8c634966495889b3176938c56d1890f9fecf871
nicod1889 <niconicodinolfo@gmail.com>
2024-07-09 19:30:15 -0300

    feat(CU-86b12czee) Add PaymentType endpoints

---
+++ back/src/main/java/com/azulyoro/back/dto/PaymentTypeRequestDto.java
@@ -0,0 +1,23 @@
+package com.azulyoro.back.dto;
+
+import com.azulyoro.back.model.PaymentCategory;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentTypeRequestDto {
+    @NotBlank(message = "{request.invalid.blank}")
+    private String name;
+    @NotNull(message = "{request.invalid.null}")
+    private PaymentCategory category;
+}
---
+++ back/src/main/java/com/azulyoro/back/dto/PaymentTypeResponseDto.java
@@ -0,0 +1,23 @@
+package com.azulyoro.back.dto;
+
+import com.azulyoro.back.model.PaymentCategory;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PaymentTypeResponseDto {
+    private Long id;
+    private String name;
+    private PaymentCategory category;
+    private Boolean isDeleted;
+}
