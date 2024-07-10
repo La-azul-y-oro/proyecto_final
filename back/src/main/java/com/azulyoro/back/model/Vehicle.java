e1555713aed697b9f1781c6c7aab8493e6dd7268
Santiago Ceron <ceronsanti@gmail.com>
2024-07-10 13:53:52 -0300

    feat(CU-86b14typg): created VehicleMapper and added Builder pattern to Vehicle class

--- back/src/main/java/com/azulyoro/back/model/Vehicle.java
+++ back/src/main/java/com/azulyoro/back/model/Vehicle.java
@@ -1,14 +1,16 @@
 package com.azulyoro.back.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
