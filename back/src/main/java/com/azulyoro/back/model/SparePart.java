7c599278b84fe66df4e7bdd5680cec67c5b19c0d
Juan <juanschiavoni713@gmail.com>
2024-07-10 17:51:44 -0300

    feat(CU-86b0yt2wr) add service entity

--- back/src/main/java/com/azulyoro/back/model/SparePart.java
+++ back/src/main/java/com/azulyoro/back/model/SparePart.java
@@ -1,14 +1,10 @@
 package com.azulyoro.back.model;
 
+import jakarta.persistence.*;
 import lombok.Setter;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,4 +23,7 @@ public class SparePart {
     private Brand brand;
 
     private String madeIn;
+
+    @ManyToMany
+    private List<Service> services;
 }
