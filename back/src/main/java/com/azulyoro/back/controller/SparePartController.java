55bb880cd700a98185875902e1c2cb03f7817b89
nicod1889 <niconicodinolfo@gmail.com>
2024-07-11 11:49:45 -0300

    feat(CU-86b14tymb) Add SparePart endpoints

---
+++ back/src/main/java/com/azulyoro/back/controller/SparePartController.java
@@ -0,0 +1,61 @@
+package com.azulyoro.back.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.azulyoro.back.dto.CustomPage;
+import com.azulyoro.back.dto.SparePartRequestDto;
+import com.azulyoro.back.dto.SparePartResponseDto;
+import com.azulyoro.back.service.EntityService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/spare_parts")
+public class SparePartController {
+    @Autowired
+    private EntityService<SparePartRequestDto, SparePartResponseDto> service;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<SparePartResponseDto>> getAll(){
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SparePartResponseDto> getById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<SparePartResponseDto> create(@Valid @RequestBody SparePartRequestDto dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SparePartResponseDto> update(@PathVariable Long id, @Valid @RequestBody SparePartRequestDto dto){
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(id, dto));
+    }
+
+    @GetMapping
+    public CustomPage<SparePartResponseDto> getSpareParts(Pageable pageable) {
+        return service.getByPage(pageable);
+    }
+}
