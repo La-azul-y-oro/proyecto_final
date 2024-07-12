fdc3611995aa534362929733b877ef6b5bddc351
Santiago Ceron <ceronsanti@gmail.com>
2024-07-12 10:41:09 -0300

    feat(CU-86b14tyw6): updated Employee entity with UserDetails implementation

--- back/src/main/java/com/azulyoro/back/model/Employee.java
+++ back/src/main/java/com/azulyoro/back/model/Employee.java
@@ -4,12 +4,17 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-public class Employee extends Person{
+public class Employee extends Person implements UserDetails {
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
@@ -19,5 +24,40 @@ public class Employee extends Person{
     @Column(nullable = false)
     private String password;
     @ManyToMany(mappedBy = "employees")
-    private List<Services> services;
+    private List<Service> services;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
