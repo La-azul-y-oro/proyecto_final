3b61f2fc8cb4bd2a7fe6074687cae6625714a98e
Santiago Ceron <ceronsanti@gmail.com>
2024-07-12 17:21:50 -0300

    feat(CU-86b14tyw6): created Jwt and Auth services, with login and auth DTO's

---
+++ back/src/main/java/com/azulyoro/back/dto/RegisterRequest.java
@@ -0,0 +1,5 @@
+package com.azulyoro.back.dto;
+
+public class RegisterRequest {
+
+}
---
+++ back/src/main/java/com/azulyoro/back/service/AuthService.java
@@ -0,0 +1,39 @@
+package com.azulyoro.back.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import com.azulyoro.back.dto.AuthResponse;
+import com.azulyoro.back.dto.LoginRequest;
+import com.azulyoro.back.model.Employee;
+import com.azulyoro.back.repository.EmployeeRepository;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtService jwtService;
+
+    public AuthResponse login(LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        Employee employee = employeeRepository.findByEmail(loginRequest.getEmail()).orElseThrow();
+
+        String token = jwtService.getToken(employee);
+
+        return AuthResponse
+                .builder()
+                .token(token)
+                .build();
+    }
+
+}
