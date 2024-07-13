23e5af412063ca737d13f0d133bb68753603624c
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-07-12 22:47:46 -0300

    feat(CU-86b18cx64): add base page, include header + sidebar

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -4,18 +4,25 @@ import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ToastModule } from 'primeng/toast';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, CardModule, ToastModule],
+  imports: [RouterOutlet, ButtonModule, CardModule, ToastModule, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front';
+  isSidebarVisible: boolean = false;
 
-  constructor(private messageService: MessageService) { }
+  toggleSidebar() {
+    this.isSidebarVisible = !this.isSidebarVisible;
+  }
+
+
+ constructor(private messageService: MessageService) { }
 
   showToast() {
     this.messageService.add({ severity: 'success', summary: 'Bienvenido', detail: 'YAY! el toast funciona' });
