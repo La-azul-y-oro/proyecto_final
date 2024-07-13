708184ef36dcf835c4a21ffea1ca4cccc7dd6090
Santiago Ceron <ceronsanti@gmail.com>
2024-07-13 20:43:41 -0300

    feat(CU-86b14tyw6): disabled CSRF, made /auth and /h2-console public (WIP)

--- back/src/main/java/com/azulyoro/back/config/SecurityConfig.java
+++ back/src/main/java/com/azulyoro/back/config/SecurityConfig.java
@@ -1,5 +1,27 @@
 package com.azulyoro.back.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
 }
