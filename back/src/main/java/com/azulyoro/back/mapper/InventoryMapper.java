725f628fb05bdf0051a0260efe7fa76a22e2ef61
nicod1889 <niconicodinolfo@gmail.com>
2024-07-14 19:36:54 -0300

    feat(CU-86b14tyfc) Add Inventory endpoints

---
+++ back/src/main/java/com/azulyoro/back/mapper/InventoryMapper.java
@@ -0,0 +1,38 @@
+package com.azulyoro.back.mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.azulyoro.back.dto.InventoryRequestDto;
+import com.azulyoro.back.dto.InventoryResponseDto;
+import com.azulyoro.back.model.Inventory;
+
+@Component
+public class InventoryMapper implements Mapper<Inventory, InventoryRequestDto, InventoryResponseDto> {
+    
+    @Autowired
+    private SupplierMapper supplierMapper;
+    @Autowired
+    private SparePartMapper sparePartMapper;
+
+    @Override
+    public InventoryResponseDto entityToDto(Inventory inventory) {
+        return InventoryResponseDto.builder()
+            .id(inventory.getId())
+            .amount(inventory.getAmount())
+            .supplier(supplierMapper.entityToDto(inventory.getSupplier()))
+            .costPrice(inventory.getCostPrice())
+            .salePrice(inventory.getSalePrice())
+            .sparePart(sparePartMapper.entityToDto(inventory.getSparePart()))
+            .build();
+    }
+
+    @Override
+    public Inventory dtoToEntity(InventoryRequestDto dto) {
+        return Inventory.builder()
+            .amount(dto.getAmount())
+            .costPrice(dto.getCostPrice())
+            .salePrice(dto.getSalePrice())
+            .build();
+    } 
+}
