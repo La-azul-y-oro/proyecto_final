f75283b160a52686d2312e38fe86f6ace1102aa6
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-07-14 23:09:45 -0300

    feat(CU-86b193ycu): add table to visualize Client data

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ClientComponent } from './pages/client/client.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'clientes', component: ClientComponent },
+];
