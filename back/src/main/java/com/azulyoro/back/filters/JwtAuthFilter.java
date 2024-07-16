26fa12abecb58d044b14a2df5eaf43c5c51dc8ee
Santiago Ceron <ceronsanti@gmail.com>
2024-07-16 16:55:08 -0300

    feat(CU-86b14tyw6): JwtAuthFilter now valids tokens, small fixes to SecurityConfig and JwtService

--- back/src/main/java/com/azulyoro/back/filters/JwtAuthFilter.java
+++ back/src/main/java/com/azulyoro/back/filters/JwtAuthFilter.java
@@ -1,32 +1,63 @@
 package com.azulyoro.back.filters;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.azulyoro.back.model.Employee;
+import com.azulyoro.back.repository.EmployeeRepository;
+import com.azulyoro.back.service.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final EmployeeRepository employeeRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         final String token = getTokenFromRequest(request);
+        final String email;
 
         if (token == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        filterChain.doFilter(request, response);
+        email = jwtService.getEmailFromToken(token);
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            Optional<Employee> employeeOptional = employeeRepository.findByEmail(email);
+
+            if (employeeOptional.isPresent()) {
+                Employee employee = employeeOptional.get();
+                if (jwtService.isTokenValid(token, employee)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(employee,
+                            null, employee.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        }
     }
 
     private String getTokenFromRequest(HttpServletRequest request) {
