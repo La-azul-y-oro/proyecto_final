8946e6e3966ff6b15148d9fa4ec6eb74a88e515a
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-07-16 23:03:32 -0300

    feat(CU-86b194005): delete client

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,15 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    MessageService, 
+    ConfirmationService,
     provideRouter(routes), 
     provideHttpClient(),
-    MessageService, 
     provideAnimations()
   ]
 };
