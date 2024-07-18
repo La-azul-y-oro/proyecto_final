3b35ece8c1a149ca2abca6d5faa3a3aa0456eb4d
nicod1889 <niconicodinolfo@gmail.com>
2024-07-18 16:53:43 -0300

    feat(CU-86b193z47) Add Vehicle table

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ClientComponent } from './pages/client/client.component';
+import { VehicleComponent } from './pages/vehicle/vehicle.component';
 
 export const routes: Routes = [
     { path: 'clientes', component: ClientComponent },
+    { path: 'vehicles', component: VehicleComponent }
 ];
