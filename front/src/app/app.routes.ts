9cf8bd7f6d48c12423c2c49c83d173dd99312fa9
Santiago Ceron <ceronsanti@gmail.com>
2024-08-01 12:21:26 -0300

    feat(CU-86b193z3n): initial layout for Services table

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ClientComponent } from './pages/client/client.component';
 import { VehicleComponent } from './pages/vehicle/vehicle.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 export const routes: Routes = [
     { path: 'clientes', component: ClientComponent },
     { path: 'vehicles', component: VehicleComponent },
-    { path: 'employees', component: EmployeeComponent }
+    { path: 'employees', component: EmployeeComponent },
+    { path: 'services', component: ServicesComponent }
 ];
---
+++ front/src/app/pages/services/services.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { PageComponent } from '../../components/page/page.component';
+import { ServiceResponse, StatusService } from '../../interfaces/model.interfaces';
+import { Column } from '../../interfaces/components.interface';
+import { ServicesService } from '../../services/services.service';
+
+@Component({
+  selector: 'app-services',
+  standalone: true,
+  imports: [ButtonModule, TableModule, PageComponent],
+  templateUrl: './services.component.html',
+  styleUrl: './services.component.css'
+})
+export class ServicesComponent implements OnInit {
+
+  title: string = "Servicios";
+  labelButtonAdd: string = "Agregar servicio";
+  status!: boolean;
+  serviceList: ServiceResponse[] = [];
+
+  columns: Column[] = [
+    {
+      header: "Servicio",
+      field: "serviceType",
+      sortable: true
+    },
+    {
+      header: "Vehículo",
+      field: "vehicle",
+      sortable: true
+    },
+    {
+      header: "Estado",
+      field: "status",
+      sortable: true
+    },
+    {
+      header: "Inicio",
+      field: "startDate",
+      sortable: false
+    },
+    {
+      header: "Fin",
+      field: "finalDate",
+      sortable: false
+    },
+    {
+      header: "Repuestos",
+      field: "spareParts",
+      sortable: false
+    },
+    {
+      header: "Empleados",
+      field: "employees",
+      sortable: false
+    },
+    {
+      header: "Precio",
+      field: "price",
+      sortable: true
+    },
+    {
+      header: "Fecha de pago",
+      field: "payDate",
+      sortable: false
+    },
+    {
+      header: "Cliente",
+      field: "client",
+      sortable: true
+    },
+  ]
+
+  constructor(private servicesService: ServicesService) {}
+
+
+  ngOnInit(): void {
+    this.loadServices();
+  }
+
+  loadServices() {
+    this.servicesService.getAll().subscribe(response => {
+      this.serviceList = response.filter(e => e.status !== StatusService.CANCELLED);
+    })
+  }
+}
+
+/**
+ export interface ServiceResponse{
+    id: number;
+    serviceType: ServiceTypeResponse;
+    status: StatusService;
+    payDate: Date;
+    price: number;
+    startDate: Date;
+    finalDate: Date;
+    vehicle: VehicleBasicResponse;
+    client: ClientBasicResponse;
+    employees: EmployeeBasicResponse;
+    spareParts: SparePartResponse;
+}
+ */
