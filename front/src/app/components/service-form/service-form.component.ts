ff393db804793d12aed7d6257097f5b91b2b26dc
Santiago Ceron <ceronsanti@gmail.com>
2024-08-02 15:53:49 -0300

    feat(CU-86b1h65jc): fixed Services table, now shows information correctly

---
+++ front/src/app/components/service-form/service-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-form.component.html',
+  styleUrl: './service-form.component.css'
+})
+export class ServiceFormComponent {
+
+}
