2fd26733582dd6f323cce1b097b128e223ae77f9
nicod1889 <niconicodinolfo@gmail.com>
2024-08-01 23:20:46 -0300

    feat(CU-86b19440v) Add front security implementation

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { VehicleComponent } from './pages/vehicle/vehicle.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
+import { authGuard, authGuardNotLogin } from './auth/auth.guard';
 
 export const routes: Routes = [
-    { path: 'clientes', component: ClientComponent },
-    { path: 'vehicles', component: VehicleComponent },
-    { path: 'login', component: LoginFormComponent},
-    { path: 'employees', component: EmployeeComponent},
-    { path: 'services', component: ServicesComponent}
+    // { path: 'inicio', component: HomeComponent, canActivate: [authGuard] }, TODO HomeComponent
+    { path: 'clientes', component: ClientComponent, canActivate: [authGuard] },
+    { path: 'vehiculos', component: VehicleComponent, canActivate: [authGuard] },
+    { path: 'login', component: LoginFormComponent, canActivate: [authGuardNotLogin] },
+    { path: 'empleados', component: EmployeeComponent, canActivate: [authGuard] },
+    { path: 'servicios', component: ServicesComponent, canActivate: [authGuard] },
+    { path: '', redirectTo: 'clientes', pathMatch: 'full' }, // TODO HomeComponent
+    { path: '**', redirectTo: 'clientes' } // TODO HomeComponent
 ];
---
+++ front/src/app/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { environment } from '../../enviroments/enviroments';
+import { EmployeeLogin } from '../interfaces/model.interfaces';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  url: string = `${environment.apiUrl}/auth`;
+
+  currentEmployeeLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  currentEmployeeData: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
+
+  constructor(private httpClient: HttpClient, private router : Router) { 
+    this.currentEmployeeLoginOn = new BehaviorSubject<boolean>(sessionStorage.getItem("token") != null);
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      this.currentEmployeeData.next(this.decodeToken(token));
+    }
+  }
+
+  login(credentials: EmployeeLogin): Observable<any> {
+    return this.httpClient.post<any>(this.url + "/login", credentials).pipe(
+      tap((employeeData) => {
+        sessionStorage.setItem("token", employeeData.token);
+        this.currentEmployeeData.next(this.decodeToken(employeeData.token));
+        this.currentEmployeeLoginOn.next(true);
+      }),
+      map((employeeData) => employeeData.token),
+      catchError(this.handleError)
+    );
+  }
+
+  logout() {
+    sessionStorage.removeItem("token");
+    this.currentEmployeeLoginOn.next(false);
+    this.currentEmployeeData.next("");
+    this.router.navigate(['/login']);
+  }
+
+  private handleError(error:HttpErrorResponse) {
+    if(error.status===0) {
+      console.error('Se ha producido un error ', error.error);
+    }
+    else {
+      console.error('Backend retornó el código de estado ', error);
+    }
+    return throwError(()=> new Error('Algo falló. Por favor intente nuevamente.'));
+  }
+
+  private decodeToken(token: string) {
+    try {
+      return jwtDecode<string>(token);
+    } catch (Error) {
+      console.error('Error decodificando token', Error);
+      return null;
+    }
+  }
+
+  get employeeToken() {
+    return this.currentEmployeeData.value;
+  }
+
+}
